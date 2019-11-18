c24de4aba6c93a78bb3c8a184784c460336a17ed
codeplayerdds <929330564@qq.com>
2019-11-18 22:31:41 +0800

    update: 增加chromedriver

---
+++ chrome_node/src/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    Key = webdriver.Key,
+    until = webdriver.until;
+
+const chrome = require("selenium-webdriver/chrome");
+const fs = require("fs");
